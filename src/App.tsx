2ac5adef886f31fe8a35842ae04f043e3f1772b6
martinbaud <martinbaud.git@gmail.com>
2026-03-22 13:40:41 +0100

    fix: use CDN for models instead of local copy

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,6 @@ function App() {
           bloomStrength={0.3}
           glowIntensity={0.5}
           countryData={showData ? DEMO_COUNTRY_DATA : undefined}
-          modelUrl="/models/atlas_hex_subdiv_7.glb"
           enableControls={true}
         />
       </div>
